779415ecc4b2a58275bf475736a0420dea39383e
Sourav Mondal <souravmondal0341@gmail.com>
2021-06-21 11:33:04 +0530

    first commit

---
+++ src/com/sapient/trn/pl/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.sapient.trn.pl;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("HelloWorld");
+
+	}
+
+}
